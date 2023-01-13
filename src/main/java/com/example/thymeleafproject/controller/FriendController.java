c6fc3b5fc829cdf97a9030075551d6c07ffec1ad
Rumi MD. Nurullah <nurullahrumi@gmail.com>
2023-01-13 22:00:27 +0600

    crud completed

--- src/main/java/com/example/thymeleafproject/controller/FriendController.java
+++ src/main/java/com/example/thymeleafproject/controller/FriendController.java
@@ -5,6 +5,9 @@ import com.example.thymeleafproject.repository.FriendRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -23,4 +26,34 @@ public class FriendController {
         mav.addObject("friends",friends);
         return mav;
     }
+
+    @GetMapping("/addFriendForm")
+    public ModelAndView addFriendForm(){
+        ModelAndView mav = new ModelAndView("add-friend-form");
+        MyFriend newFriend = new MyFriend();
+        mav.addObject("friend",newFriend);
+        return mav;
+    }
+
+    @PostMapping("/saveFriend")
+    public String saveFriend(@ModelAttribute MyFriend friend){
+        friendRepository.save(friend);
+        return "redirect:/friends";
+    }
+
+    @GetMapping("/showUpdateForm")
+    public ModelAndView showUpdateFrom(@RequestParam Long friendId){
+        ModelAndView mav = new ModelAndView("add-friend-form");
+        MyFriend thisFriend = friendRepository.findById(friendId).get();
+        mav.addObject("friend",thisFriend);
+        return mav;
+    }
+
+    @GetMapping("/deleteFriend")
+    public String deleteFriend(@RequestParam Long friendId){
+        friendRepository.deleteById(friendId);
+        return "redirect:/friends";
+    }
+
+
 }
